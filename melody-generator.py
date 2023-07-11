669bcb97fcfe044dea706450842303d08e9502f5
Austin Boone <84555510+AusBoone@users.noreply.github.com>
2023-07-11 18:24:55 -0400

    Script is now more robust and user-friendly
    Added:
    
    - Error Handling: Added error handling for when a user enters a BPM, time signature, or number of notes that are not integers. The program now shows an error message and returns to the main GUI instead of crashing.
    
    - Key Validation: Added error handling for when a user enters a key that is not present in the dictionary of keys and scales. The program now shows an error message and returns to the main GUI.
    
    - Note to MIDI Conversion: Fixed a bug in the 'note_to_midi' function that incorrectly assumed note names would always be 3 characters long. The function now correctly handles notes without a sharp sign (e.g., 'C4' vs 'C#4').
    
    - Time Signature Input: Changed the time signature input from a single field to two separate fields for the numerator and denominator. This makes it clearer for the user and avoids potential formatting errors.
    
    - Code Comments: Updated the function comments to use Python's docstring conventions, which can be used to automatically generate documentation.
    
    - GUI Improvements: Added error prompts for invalid input to make the application more user-friendly.

--- melody-generator.py
+++ melody-generator.py
@@ -14,20 +14,26 @@ from mido import Message, MidiFile, MidiTrack
 import random
 import sys
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 
 def note_to_midi(note):
     """
     Convert a note name to a MIDI note number.
+
+    Args:
+        note (str): The note name (e.g., 'C#4').
+
+    Returns:
+        int: The MIDI note number.
     """
     # List of note names
     notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
     
     # Extract octave number from note name and adjust it for MIDI format
-    octave = int(note[2]) + 2
+    octave = int(note[-1]) + 2
     
     # Find the index of the note in the notes list
-    note_idx = notes.index(note[:2])
+    note_idx = notes.index(note[:-1])
     
     # Calculate and return the MIDI note number
     return note_idx + (octave * 12)
@@ -35,6 +41,13 @@ def note_to_midi(note):
 def generate_melody(key, num_notes):
     """
     Generate a random melody in the given key.
+
+    Args:
+        key (str): The musical key (e.g., 'C', 'F#m').
+        num_notes (int): The number of notes in the melody.
+
+    Returns:
+        list: The generated melody as a list of note names.
     """
     # Dictonary defining scales for the available keys
     scale = {
@@ -77,6 +90,12 @@ def generate_melody(key, num_notes):
 def create_midi_file(melody, bpm, time_signature, output_file):
     """
     Create a MIDI file with the given melody, BPM, and time signature.
+
+    Args:
+        melody (list): The melody as a list of note names.
+        bpm (int): The BPM (beats per minute).
+        time_signature (tuple): The time signature as a tuple (numerator, denominator).
+        output_file (str): The path to the output MIDI file.
     """
     # Set ticks per beat (resolution) for the MIDI file
     ticks_per_beat = 480
@@ -118,10 +137,18 @@ def main():
     def generate_button_click():
         # Retrieve user input from the GUI elements
         key = key_entry.get()
-        bpm = int(bpm_entry.get())
-        time_signature = tuple(map(int, time_signature_entry.get().split()))
-        num_notes = int(num_notes_entry.get())
-        
+        try:
+            bpm = int(bpm_entry.get())
+            time_signature = (int(time_signature_numerator_entry.get()), int(time_signature_denominator_entry.get()))
+            num_notes = int(num_notes_entry.get())
+        except ValueError:
+            messagebox.showerror("Input Error", "BPM, Time Signature and Number of notes should be integers.")
+            return
+
+        if key not in generate_melody.keys():
+            messagebox.showerror("Input Error", "Invalid key.")
+            return
+
         # Open a file dialog for the user to choose the output MIDI file location
         output_file = filedialog.asksaveasfilename(defaultextension=".mid", filetypes=[("MIDI files", "*.mid")])
         if output_file:
@@ -145,11 +172,13 @@ def main():
     bpm_entry = tk.Entry(root)
     bpm_entry.grid(row=1, column=1)
 
-    # Create and position the "Time signature" label and entry field
+    # Create and position the "Time signature" label and entry fields
     time_signature_label = tk.Label(root, text="Time signature:")
     time_signature_label.grid(row=2, column=0)
-    time_signature_entry = tk.Entry(root)
-    time_signature_entry.grid(row=2, column=1)
+    time_signature_numerator_entry = tk.Entry(root)
+    time_signature_numerator_entry.grid(row=2, column=1)
+    time_signature_denominator_entry = tk.Entry(root)
+    time_signature_denominator_entry.grid(row=2, column=2)
 
     # Create and position the "Number of notes" label and entry field
     num_notes_label = tk.Label(root, text="Number of notes:")
